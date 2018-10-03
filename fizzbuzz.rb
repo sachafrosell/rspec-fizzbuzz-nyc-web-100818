aae7311092314d19441f426e5e5a0604e189cee8
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 18:29:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,5 @@
 def fizzbuzz(num)
-  if (num / 3).is a? Integer && (num / 5).is a? Integer
-    return "FizzBuzz"
-  elsif (num / 3).is a? Integer
-    return "Fizz"
-  elsif (num / 5).is a? Integer
-    return "Buzz"
-  else
-    return nil
-  end
+ 
 end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
